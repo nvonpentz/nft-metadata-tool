a19e7dd279e4d88be8d6976d51a7bbb46c466e85
nvonpentz <12549658+nvonpentz@users.noreply.github.com>
2023-01-08 12:31:18 -0500

    remove min/max tokenID values in form

--- lib/dataUri.ts
+++ lib/dataUri.ts
@@ -15,13 +15,10 @@ export const parseDataUri = (dataUri: string): { mimeType: string, encoding: str
 }
 
 export const decodeDataUri = (dataUri: string): string => {
-  // parse the uri into mime type, encoding and buffer
   const { mimeType, encoding, buffer } = parseDataUri(dataUri);
   if (encoding === 'utf8') {
-    // console.log('utf8 encoding', buffer);
     return buffer;
   } else if (encoding === 'base64') {
-    // return Buffer.from(buffer, 'base64').toString('utf8');
     return atob(buffer);
   } else {
     throw new Error('Unknown encoding: ' + encoding);

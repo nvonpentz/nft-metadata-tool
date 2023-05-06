03058d40fad7273fae1d1041ef808621701e4e4a
nvonpentz <12549658+nvonpentz@users.noreply.github.com>
2023-05-06 14:36:41 -0400

    Add support for URI encoded JSON (no base64)

--- lib/dataUri.ts
+++ lib/dataUri.ts
@@ -7,8 +7,10 @@ export const parseDataUri = (dataUri: string): { mimeType: string, encoding: str
 
   if (metadata[1] === 'base64') {
     result.encoding = 'base64';
-  } else {
+  } if (metadata[1] === 'utf8') {
     result.encoding = 'utf8';
+  } else {
+    result.encoding = 'uri';
   }
   result.buffer = dataUri.split(parts[0] + ',')[1];
   return result;
@@ -18,6 +20,9 @@ export const decodeDataUri = (dataUri: string): string => {
   const { mimeType, encoding, buffer } = parseDataUri(dataUri);
   if (encoding === 'utf8') {
     return buffer;
+  } else if (encoding === 'uri') {
+    // E.g. ?tokenId=414&contractAddress=0x0c60b40289ff15ff6afdfa668d1a743dc6e53cf3&network=mainnet
+    return decodeURIComponent(buffer);
   } else if (encoding === 'base64') {
     return atob(buffer);
   } else {
